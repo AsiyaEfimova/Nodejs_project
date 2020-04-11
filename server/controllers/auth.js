906433d68c8f445037712f9257981eb32a386cd5
a.efimova <asi4ka.84@gmail.com>
2020-04-11 16:06:00 +0300

    добавила логин

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -23,25 +23,27 @@ module.exports.registration = async function (req, res) {
     const result = await UserApi.add(req.body);
     res.json(result);
 }
-module.exports.login = function (req, res) {
+module.exports.login = async function (req, res) {
     // POST-запрос на /api/login - авторизация после пользователького ввода. Cигнатура запроса: { username, password } Необходимо вернуть объект авторизовавшегося пользователя.
-    res.json({
-        firstName: String,
-        id: 'Primary key',
-        image: String,
-        middleName: String,
-        permission: {
-            chat: { C: Boolean, R: Boolean, U: Boolean, D: Boolean },
-            news: { C: Boolean, R: Boolean, U: Boolean, D: Boolean },
-            settings: { C: Boolean, R: Boolean, U: Boolean, D: Boolean }
-        },
-        surName: String,
-        username: String,
-        accessToken: String,
-        refreshToken: String,
-        accessTokenExpiredAt: Date(ms),
-        refreshTokenExpiredAt: Date(ms)
-    });
+    console.log(req.body);
+    const { username, password } = req.body;
+    const user = await UserApi.getByLogin(username);
+    if (user === null) {
+        res.status(400);
+        res.json({
+            code: '400',
+            message: "Invalid login or password"
+        });
+    }
+    if (user.validPassword(password)) {
+        res.json(user)
+    } else {
+        res.status(400);
+        res.json({
+            code: '400',
+            message: "Invalid login or password"
+        })
+    }
 }
 module.exports.refreshtoken = function (req, res) {
     // POST-запрос на /api/refresh-token - обновление access-токена. В headers['authorization'] прикрепить refresh-токен. Вернуть обьект с токенами
--- server/db/api/users.js
+++ server/db/api/users.js
@@ -10,12 +10,21 @@ module.exports.getById = function (id) {
     return Users.findOne({ _id: id })
 }
 
+module.exports.getByLogin = function (login) {
+    return Users.findOne({ username: login })
+}
+
 module.exports.add = function (data) {
     const User = new Users({
         firstName: data.firstName,
         middleName: data.middleName,
         surName: data.surName,
-        username: data.username
+        username: data.username,
+        permission: {
+            chat: { C: true, R: true, U: true, D: true },
+            news: { C: true, R: true, U: true, D: true },
+            settings: { C: true, R: true, U: true, D: true }
+        }
     });
     User.setPassword(data.password);
     return User.save()
