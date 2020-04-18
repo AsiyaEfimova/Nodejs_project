d910e5027a94add0ed778e4b7c3ba883489272d7
a.efimova <asi4ka.84@gmail.com>
2020-04-18 13:00:27 +0300

    build for heroku

---
+++ build/precache-manifest.3f05febc99bd49c40ce6566377a91715.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111ac7ddcaccb90450a6adece0fd01b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5d518b90abca729763",
+    "url": "/static/js/2.0f2c2249.chunk.js"
+  },
+  {
+    "revision": "fb0057e649da05a1cc4c",
+    "url": "/static/js/main.6d8caf9c.chunk.js"
+  },
+  {
+    "revision": "32996fb276b3e890a01d",
+    "url": "/static/js/runtime~main.01574ed8.js"
+  }
+]);
